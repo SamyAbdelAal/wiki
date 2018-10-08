bc6cb28714647fe18a15029701824979e889437b
Sazida Hossain <sazidahossain@Sazidas-MacBook-Pro.local>
2018-10-08 15:09:00 +0300

    Mini Project Wiki submission

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	last_update= models.DateTimeField()
+	def get_absolute_url(self):
+		return reverse('page-detail', args=[str(self.id)])
+
